3d5753ffbb6170fb912a41b9472080a6a05b70de
Nyelanioustour <shawn.k.alexander@gmail.com>
2020-08-13 11:35:31 -0400

    first commit

---
+++ trivia-app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <section className="App">
+      <header className="App-header">
+
+      </header>
+
+    </section>
+  );
+}
+
+export default App;

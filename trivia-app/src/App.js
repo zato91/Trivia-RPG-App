b8ddc5f066616d9247750165ef0a0cbd1cc5e7a0
zato91 <zatokey@gmail.com>
2020-08-14 09:44:14 -0400

    front fetch characters

--- trivia-app/src/App.js
+++ trivia-app/src/App.js
@@ -2,20 +2,44 @@ import React, { useEffect } from 'react';
 import './App.css';
 import Navbar from './components/Navbar.js';
 import QuestionSection from './components/QuestionSection.js';
+import CharacterSection from './components/CharacterSection.js';
 
 
-function App() {
-  return (
-    <section className="App">
-      <header className="App-header">
-        <Navbar/>
-      </header>
-      <div>
-        <CharacterSection/>
-        <QuestionSection/>
-      </div>
-    </section>
-  );
+class App extends React.Component {
+
+  state ={
+    characters: [],
+  }
+
+  componentDidMount () {
+
+    fetch('http://localhost:3000/characters')
+        .then(response => response.json())
+        .then( characters => { 
+          this.setState({
+            characters
+          })
+          console.log(characters[0].name)
+        })
+  }
+
+
+
+  render() {
+    return (
+      <section className="App">
+        <header className="App-header">
+          <Navbar/>
+        </header>
+        <div>
+          <CharacterSection
+          characters={this.state.characters[0]}
+          />
+          <QuestionSection/>
+        </div>
+      </section>
+    );
+  }
 }
 
 export default App;
--- trivia-app/src/components/CharacterSection.js
+++ trivia-app/src/components/CharacterSection.js
@@ -3,9 +3,22 @@ import React, { useEffect, useState } from 'react';
 
 function CharacterSection (props){
 
+   let {characters} = props
+//    console.log(characters.img_url)
+// console.log(characters)
+
     return (
         <div className="character-section">
-        This is the character
+            
+            <div id="character-img">
+                {/* <img src={characters.img_url}  />   */}
+            </div>
+            <div className="stats"> 
+                <p>Stats: </p>
+                <p>Hp: </p>
+                <p>Ac </p>
+
+            </div>
         </div>
     );
       

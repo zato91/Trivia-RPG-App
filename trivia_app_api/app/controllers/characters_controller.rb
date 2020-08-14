192be6598c0a2855a45cd9803ef48ba3c875cf4d
zato91 <zatokey@gmail.com>
2020-08-13 22:03:11 -0400

    setup backend with front

--- trivia_app_api/app/controllers/characters_controller.rb
+++ trivia_app_api/app/controllers/characters_controller.rb
@@ -1,2 +1,13 @@
 class CharactersController < ApplicationController
+
+    def index 
+        
+        characters = Character.all
+        render json: characters
+    end
+
+    def show 
+        characters  = Character.find(params[:id])
+        render json: character
+    end
 end

89565c239ec6f9c4845229f2a15e351aa4d7f6c1
Nyelanioustour <shawn.k.alexander@gmail.com>
2020-08-17 09:54:46 -0400

    before push

--- trivia_app_api/db/seeds.rb
+++ trivia_app_api/db/seeds.rb
@@ -6,10 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Character.create(name:"Axel",bio:"Fighting Style: Balanced",tagline:"Come back when you've learned how to fight!",max_hp:15,armor:10,energy:2)
-Character.create(name:"Blaze",bio:"Fighting Style: Finesse",tagline:"I hope I didn't hurt your ego too badly... Oops!",max_hp:10,armor:10,energy:3)
-Character.create(name:"Diva",bio:"Fighting Style: Ability Based",tagline:"You haven't felt my full power.",max_hp:10,armor:5,energy:4)
-Character.create(name:"Floyd",bio:"Fighting Style: Brute Force",tagline:"Can't you see how pathetic your attacks are!",max_hp:20,armor:15,energy:1)
+Character.create(name:"Axel",bio:"Fighting Style: Balanced",tagline:"Come back when you've learned how to fight!",max_hp:15,armor:10,energy:2, img_url:"./images/Chars/Axel_Avatar.png")
+Character.create(name:"Blaze",bio:"Fighting Style: Finesse",tagline:"I hope I didn't hurt your ego too badly... Oops!",max_hp:10,armor:10,energy:3, img_url: "./images/Chars/Blaze_Avatar.png")
+Character.create(name:"Diva",bio:"Fighting Style: Ability Based",tagline:"You haven't felt my full power.",max_hp:10,armor:5,energy:4, img_url: "./images/Chars/Diva_Avatar.png")
+Character.create(name:"Floyd",bio:"Fighting Style: Brute Force",tagline:"Can't you see how pathetic your attacks are!",max_hp:20,armor:15,energy:1, img_url: "./images/Chars/Floyd_Avatar.png")
 
 Power.create(name:"Dragon Wing", effect:1)
 Power.create(name:"Shoryuken", effect:2)

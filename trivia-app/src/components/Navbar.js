7a1b61e9f120a7840062d1aec1519929d5bfccb3
zato91 <zatokey@gmail.com>
2020-08-17 22:20:07 -0400

    log

--- trivia-app/src/components/Navbar.js
+++ trivia-app/src/components/Navbar.js
@@ -4,13 +4,28 @@ import LoginModal from './LoginModal';
 import {Image} from 'semantic-ui-react'
 
 function Navbar(props) {
+
+  let logout = () => {
+    localStorage.clear();
+    window.location.href = '/';
+}
+
+let buttonDisplay = () =>{
+  if (!localStorage.token){
+    return <> <SignUpModal getCharacters={props.getCharacters}/> <LoginModal getCharacters={props.getCharacters}/> </>
+  }else {
+    return <button onClick={logout} class="ui button" id="logout">Logout</button>
+  }
+}
     return (
       <section className="navbar">
         <Image src='./images/title_icon.png' height='32px' className="title-image"/>
         <h1 className="app-title">Trivia Fight</h1>
         <div className="signup-buttons">
-        <SignUpModal getCharacters={props.getCharacters}/> 
-        <LoginModal getCharacters={props.getCharacters}/>
+          { 
+        buttonDisplay()
+        
+          }
         </div>
       </section>
     );
